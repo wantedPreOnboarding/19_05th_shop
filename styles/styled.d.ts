d4a6b9b94abf9df1cee573f9164107d991352a48
DABIN JEONG <41@b41.kr>
2022-02-14 22:23:56 +0900

    Modify - Theme type 변경
    - Theme의 타입을 String에서 해당 값으로 입력하여 theme을 선택할 때 해당 값을 바로 보일 수 있도록 수정

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -3,27 +3,27 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
-      black: string;
-      white: string;
-      red: string;
-      discountGray: string;
-      borderGray: string;
-      borderRed: string;
-      backgroundOutsideGray: string;
-      backgroundInsideGray: string;
-      sideMenuRed: string;
+      black: '#333';
+      white: '#FFFFFF';
+      red: 'red';
+      discountGray: '#999';
+      borderGray: '#ccc';
+      borderRed: '#f75656';
+      backgroundOutsideGray: '#DDDDDD';
+      backgroundInsideGray: '#EEEEEE';
+      sideMenuRed: '#ff5757';
     };
     fontSize: {
-      xl: string; //상품명, 가격, 고객센터 상담시간 안내 타이틀
-      l: string; //타이틀, 고객상담글 내용
-      m: string; //할인 가격, 고객센터 소제목, 환불규정 제목
-      s: string;
-      xs: string; //카테고리 메뉴, 환불규정 및 유의사항 내용
+      xl: '16px'; //상품명, 가격, 고객센터 상담시간 안내 타이틀
+      l: '15px'; //타이틀, 고객상담글 내용
+      m: '14px'; //할인 가격, 고객센터 소제목, 환불규정 제목
+      s: '13px';
+      xs: '12px'; //카테고리 메뉴, 환불규정 및 유의사항 내용
     };
     fontWeight: {
-      strong: number;
-      semiStrong: number;
-      normal: string;
+      strong: 600;
+      semiStrong: 500;
+      normal: 'normal';
     };
   }
 }
