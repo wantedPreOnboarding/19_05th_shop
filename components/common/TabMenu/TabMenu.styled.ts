a35737fe4778cfb63c8f6cdc151f17b0a6dd083e
DABIN JEONG <41@b41.kr>
2022-02-15 00:46:10 +0900

    Style - 탭 메뉴 스타일링

--- components/common/TabMenu/TabMenu.styled.ts
+++ components/common/TabMenu/TabMenu.styled.ts
@@ -1,13 +1,39 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
+import { theme } from 'styles/theme';
 
-export const TabMenuWrapper = styled.div`
+export const TabMenuContainer = styled.div`
+  /* display: flex; */
   width: 100%;
-  border-bottom: solid 1px;
+  height: 41px;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  white-space: nowrap;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-sizing: border-box;
+  //스크롤 바 숨기기
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
-export const TabMenuItem = styled.button`
+export const TabMenuWrapper = styled.div`
+  padding: 0 10px;
+  height: 41px;
+  border-bottom: solid 1px ${({ theme }) => theme.colors.borderGray};
+  /* box-sizing: border-box; */
+`;
+
+export const TabMenuItem = styled.button<{ isSelect?: boolean }>`
   all: unset;
-  padding: 10px 14px 9px;
-  color: ${({ theme }) => theme.colors.black};
-  font-size: ${({ theme }) => theme.fontSize.s};
+  height: 20px;
+  padding: 10px 14px ${props => (props.isSelect ? 9 : 10)}px;
+  color: ${props =>
+    props.isSelect ? ({ theme }) => theme.colors.red : ({ theme }) => theme.colors.black};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  border-bottom: ${props =>
+    props.isSelect ? '2px solid ' + theme.colors.red : '1px solid ' + theme.colors.borderGray};
+  cursor: pointer;
+  /* box-sizing: content-box; */
 `;
--- components/common/TabMenu/TabMenu.tsx
+++ components/common/TabMenu/TabMenu.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import * as S from './TabMenu.styled';
 
 const TabMenu = () => {
-  return <div>TabMenu</div>;
+  return (
+    <div>
+      <S.TabMenuContainer>
+        <S.TabMenuWrapper>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem isSelect>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+          <S.TabMenuItem>abc</S.TabMenuItem>
+        </S.TabMenuWrapper>
+      </S.TabMenuContainer>
+    </div>
+  );
 };
 
 export default TabMenu;
