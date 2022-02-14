cbf85bcfc6c89342bf83acc641535a32227c7f5d
hyun <vi2920va@gmail.com>
2022-02-15 01:36:46 +0900

    Add - Brand Wrap 레이아웃

---
+++ components/brands/Brand/Brand.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as S from './Brand.styled';
+
+const Brand = () => {
+  return (
+    <S.List>
+      <S.Item>1</S.Item>
+      <S.Item>2</S.Item>
+      <S.Item>3</S.Item>
+      <S.Item>4</S.Item>
+    </S.List>
+  );
+
+};
+
+export default Brand;
