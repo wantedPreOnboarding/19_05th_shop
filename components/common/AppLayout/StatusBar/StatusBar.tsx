54235f1fa5976fe3d5d9f59965c2e44fbf79d729
yeongjong <tohero21@naver.com>
2022-02-15 15:39:55 +0900

    Modify - env 변수명 변경에 따라 사용부 전체 수정

--- components/common/AppLayout/StatusBar/StatusBar.tsx
+++ components/common/AppLayout/StatusBar/StatusBar.tsx
@@ -14,25 +14,25 @@ const StatusBar = () => {
           <S.SettingIcons>
             <span>1:20</span>
             <S.SettingIcon
-              src={`${process.env.REACT_APP_ASSET_PATH}/location.svg`}
+              src={`${process.env.ASSET_PATH}/location.svg`}
               width="10.54px"
               height="10.54px"
             />
           </S.SettingIcons>
           <S.SettingIcons>
             <S.SettingIcon
-              src={`${process.env.REACT_APP_ASSET_PATH}/fullBars.svg`}
+              src={`${process.env.ASSET_PATH}/fullBars.svg`}
               width="12px"
               height="8px"
             />
             <S.SettingIcon
-              src={`${process.env.REACT_APP_ASSET_PATH}/wifi.svg`}
+              src={`${process.env.ASSET_PATH}/wifi.svg`}
               width="14.25px"
               height="10px"
             />
 
             <S.SettingIcon
-              src={`${process.env.REACT_APP_ASSET_PATH}/battery.svg`}
+              src={`${process.env.ASSET_PATH}/battery.svg`}
               width="20px"
               height="7.5px"
             />
