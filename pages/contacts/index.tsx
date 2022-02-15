be77de377c6df0cd7d125ace0c3c2cf378b4f0c4
DABIN JEONG <41@b41.kr>
2022-02-15 19:18:05 +0900

    Modify - contacts 페이지 파일 contacts폴더로 이동

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppLayout } from 'components/common';
 import { SERVICE_CENTER } from 'consts/constants';
+import * as S from './contacts.style';
 const Contacts = () => {
   return (
     <AppLayout title={SERVICE_CENTER} backPath="/">
