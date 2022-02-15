bccb331d7aa809edc6715e07fc83aa069dfa035e
yeongjong <tohero21@naver.com>
2022-02-16 04:08:15 +0900

    Add - OptionSelector, SelectorOptionViewer index.ts에 추가

--- components/items/index.ts
+++ components/items/index.ts
@@ -1,1 +1,3 @@
 export { default as TextsSection } from './TextsSection/TextsSection';
+export { default as OptionSelector } from './OptionSelector/OptionSelector';
+export { default as SelectedOptionViewer } from './SelectedOptionViewer/SelectedOptionViewer';
