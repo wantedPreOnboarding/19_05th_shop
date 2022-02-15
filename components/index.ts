d97b33bc6e21093adf26abaf966a6c9b29817356
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-15 12:17:00 +0900

    Modify 컴포넌트 index 삭제 및 컴포넌트/common index생성

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AppLayout } from './common/AppLayout/AppLayout';
-export { default as ChildrenContent } from './common/AppLayout/ChildrenContent/ChildrenContent';
-export { default as StatusBar } from './common/AppLayout/StatusBar/StatusBar';
-export { default as DiscountCon } from './DiscountCon/DiscountCon';
-export { default as MainCategories } from './MainCategories/MainCategories';
