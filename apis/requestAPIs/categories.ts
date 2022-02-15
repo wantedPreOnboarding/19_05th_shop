68cc0e774c13c1141da9a729c5eae0c80cc1529c
yeongjong <tohero21@naver.com>
2022-02-16 01:45:04 +0900

    Modify - categories 슬러그 매처 적용

--- apis/requestAPIs/categories.ts
+++ apis/requestAPIs/categories.ts
@@ -1,7 +1,9 @@
+import { URL } from 'apis/consts';
+import slugMatcher from 'apis/utils/slugMatcher';
 import requests from '../core';
 import { CategoriesAPIType } from '../models/Categories.type';
 
 export const get = {
   categories: (category1Id: string) =>
-    requests.get<CategoriesAPIType>(`/con-category1s/${category1Id}/nested`),
+    requests.get<CategoriesAPIType>(slugMatcher(URL.BRAND_ITEM, [category1Id])),
 };
