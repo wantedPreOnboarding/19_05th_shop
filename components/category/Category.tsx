989ad190f63fcc1b88c2d98255861a90f4bc0a11
hyun <vi2920va@gmail.com>
2022-02-15 21:23:55 +0900

    Modify - import 순서 변경

--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import CategoryProps from './Category.type';
 import * as S from './Category.styled';
 
 const Category = ({ item }: CategoryProps) => {
-
+  const router = useRouter();
+  console.log(item);
   return (
     <>
-      <S.Item>
+      <S.Item onClick={() => router.push('id')}>
         <S.ImgWrapper>
           <img src={item.imageUrl} alt={item.imageUrl} />
           <span>{item.name}</span>
