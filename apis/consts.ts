14c8b81c85f79c02761685760500ff0d3a373adc
DABIN JEONG <41@b41.kr>
2022-02-15 23:19:04 +0900

    Modify - tabMenu 일부 value 상수로 수정
    - category
    - faq

--- apis/consts.ts
+++ apis/consts.ts
@@ -3,6 +3,9 @@ const URL = {
   DISCOUNT_ITEM: '/con-items/soon',
   BRAND_ITEM: '/con-category1s/:conCategory1Id/nested',
   ITEMS: '/con-items/:conItemId',
+  CONTACT_TYPE: '/qaTypes',
+  QA_PURCHASE: 'https://api2.ncnc.app/qas?qaTypeId=1',
+  QA_SALE: 'https://api2.ncnc.app/qas?qaTypeId=2',
 };
 
 export { URL };
