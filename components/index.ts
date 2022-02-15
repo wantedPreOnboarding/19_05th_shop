37748ae549d17ab0fb68947f9719af3febdf16a4
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-15 12:11:06 +0900

    Add common index에 컴포넌트 추가

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { default as AppLayout } from './common/AppLayout/AppLayout';
 export { default as ChildrenContent } from './common/AppLayout/ChildrenContent/ChildrenContent';
 export { default as StatusBar } from './common/AppLayout/StatusBar/StatusBar';
-export { default as TitleBar } from './common/AppLayout/TitleBar/TitleBar';
+export { default as DiscountCon } from './DiscountCon/DiscountCon';
+export { default as MainCategories } from './MainCategories/MainCategories';
