4feab5ee5116f1bc9e473843476a0acd954ed78e
yeongjong <tohero21@naver.com>
2022-02-16 04:10:50 +0900

    Add - 옵션 선택과 관련되어 실행할 함수 prop handleSelect 추가

--- components/items/OptionSelector/OptionSelector.tsx
+++ components/items/OptionSelector/OptionSelector.tsx
@@ -4,26 +4,29 @@ import { commaNumber } from 'uilts';
 import * as S from './OptionSelector.styled';
 import OptionSelectorProps from './OptionSelector.type';
 
-const OptionSelector = ({ options }: SCProps<OptionSelectorProps>): ReactElement => {
+const OptionSelector = ({ options, handleSelect }: SCProps<OptionSelectorProps>): ReactElement => {
   return (
     <S.OptionSelector>
       <label htmlFor="item-select">옵션 선택하기</label>
       <S.Select id="item-select" role="select">
-        {options.map(({ count, expireAt, sellingPrice, discountRate }, index) => (
-          <S.Option key={index} role="option">
-            <S.OptionTitleContentWrapper>
-              <div>
-                <S.OptionTitle>유효기간</S.OptionTitle>
-                <S.OptionContent>{expireAt} 까지</S.OptionContent>
-              </div>
-              <div>
-                <S.OptionTitle>할인가</S.OptionTitle>
-                <S.OptionContent>{commaNumber(sellingPrice)}원</S.OptionContent>
-              </div>
-            </S.OptionTitleContentWrapper>
-            <S.OptionDiscountRate>{discountRate}%</S.OptionDiscountRate>
-          </S.Option>
-        ))}
+        {options.map(
+          ({ count, expireAt, sellingPrice, discountRate, id }) =>
+            count && (
+              <S.Option key={id} role="option" onClick={() => handleSelect(id)}>
+                <S.OptionTitleContentWrapper>
+                  <div>
+                    <S.OptionTitle>유효기간</S.OptionTitle>
+                    <S.OptionContent>{expireAt} 까지</S.OptionContent>
+                  </div>
+                  <div>
+                    <S.OptionTitle>할인가</S.OptionTitle>
+                    <S.OptionContent>{commaNumber(sellingPrice)}원</S.OptionContent>
+                  </div>
+                </S.OptionTitleContentWrapper>
+                <S.OptionDiscountRate>{discountRate}%</S.OptionDiscountRate>
+              </S.Option>
+            ),
+        )}
       </S.Select>
     </S.OptionSelector>
   );
--- components/items/OptionSelector/OptionSelector.type.ts
+++ components/items/OptionSelector/OptionSelector.type.ts
@@ -1,5 +1,5 @@
 import { Option, ConItem } from 'apis/models/Items.type';
-
 export default interface OptionSelectorProps {
-  options: (Option & Pick<ConItem, 'discountRate'>)[];
+  options: (Option & Pick<ConItem, 'discountRate'> & { id: number })[];
+  handleSelect: (id: number) => void;
 }
