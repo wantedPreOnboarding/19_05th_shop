07abc8e9f2181034c89ee4b7a54910c43549bc7b
yeongjong <tohero21@naver.com>
2022-02-21 02:04:57 +0900

    Add - 테스트 단위 분할 및 상세 주석

--- cypress/integration/items.spec.ts
+++ cypress/integration/items.spec.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 describe('상품 구매 페이지 테스트', () => {
-  it('마운팅 및 클릭 이벤트 테스트', () => {
+  it('상품 구매 페이지로 이동', () => {
     // root
     cy.visit('http://localhost:3000/');
 
@@ -13,13 +13,15 @@ describe('상품 구매 페이지 테스트', () => {
 
     // items
     cy.get('div[class*=ItemThumbnailstyled__ItemThumbnailWrapper]').first().click();
+  });
 
-    //
+  it('옵션 선택 및 구매 하기 테스트', () => {
     cy.get('button[class*=FullWidthButtonstyled__FullWidthButton]').contains('옵션 선택하기');
     cy.get('button[class*=FullWidthButtonstyled__FullWidthButton]').first().click();
     cy.get('button[class*=FullWidthButtonstyled__FullWidthButton]').contains('구매하기');
     cy.get('div[class*=OptionSelectorstyled__Option]').first().click();
     cy.get('div[class*=SelectedOptionViewerstyled__InnerWrapper]').first().click();
-    cy.get('div[class*=BackDropstyled__BackDrop-sc]').first().click();
   });
+
+  // 백드롭 클릭시 옵션 선택 창 닫기 테스트 필요
 });
