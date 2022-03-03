11ae60d06f98a6fb6ebed078e9c29a314bd7d535
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-03 19:55:49 +0900

    refactor 땡처리 게시판 형식으로 변경

--- pages/categories/[category1Id].tsx
+++ pages/categories/[category1Id].tsx
@@ -5,6 +5,7 @@ import * as S from 'components/categories/Categories.styled';
 import { GetServerSideProps } from 'next';
 import { get } from 'apis/requestAPIs/categories';
 import { MainCategory } from 'apis/models/Home.type';
+import { ItemThumbnail } from 'components/common';
 
 const Categories = ({ data, name }: CategoriesProps) => {
   const [tab, setTab] = useState<MainCategory[]>([]);
@@ -18,12 +19,21 @@ const Categories = ({ data, name }: CategoriesProps) => {
 
   return (
     <AppLayout title={name}>
-      <TabMenu menuData={tab} tabType="category" />
+    <TabMenu menuData={tab} tabType="category" />
+     {name==="땡철이"?data.map(item=>{
+      return(
+      item&&item.conItems?.map(innerItem=>{
+      const discountItem={brand:item.name, href:`/items/${innerItem.id}`,imageUrl:innerItem.imageUrl,itemName:innerItem.name,discountRate:innerItem.discountRate,minSellingPrice:innerItem?.minSellingPrice,originalPrice:innerItem.originalPrice}
+      return ( 
+          <S.Box key={discountItem.itemName}>
+            <ItemThumbnail {...discountItem} />
+          </S.Box>);
+      }))})
+      :
       <S.List>
         {data.map(item => (
           <Category page="brands" key={item.id} item={item} />
-        ))}
-      </S.List>
+        ))}</S.List>}
     </AppLayout>
   );
 };
