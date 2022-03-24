a7a112c7f949f8e6a3ee24c1f7b48daa7a34ba85
yeongjong <tohero21@naver.com>
2022-03-25 02:33:49 +0900

    Fix - ViewPort meta tag를 _document에서 사용시 waring 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta
             name="description"
             content="원티드X위코드 프론트엔드 프리온보딩 2기 취업하구19조 5번째 프로젝트입니다."
