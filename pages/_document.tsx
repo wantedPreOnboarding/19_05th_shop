5f5299fae2b3ce2c6872e8bf49886422c52c2ce5
yeongjong <tohero21@naver.com>
2022-05-10 23:08:46 +0900

    Add - 구글 검색앤진 등록을 위한 메타태그 등록

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,10 @@ class MyDocument extends Document {
             content="원티드X위코드 프론트엔드 프리온보딩 2기 취업하구19조 5번째 프로젝트입니다."
           />
           <meta name="naver-site-verification" content="0d0b1952de4c95c8c2f3a016187ecfca40be3d52" />
+          <meta
+            name="google-site-verification"
+            content="hjFGrkqo15Ksr4rQvgs_PuzRnN8Zky0vSU8lbP-Pyy0"
+          />
         </Head>
         <body>
           <Main />
